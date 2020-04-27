da2ff116ee1b44d7be40f49948ae7c96ad89646c
TjSanshao <1305705188@qq.com>
2020-04-27 11:28:19 +0800

    fix bug

--- blog-server/src/main/java/cn/tjsanshao/blogserver/BlogServerApplication.java
+++ blog-server/src/main/java/cn/tjsanshao/blogserver/BlogServerApplication.java
@@ -3,12 +3,14 @@ package cn.tjsanshao.blogserver;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * @author TjSanshao
  */
 @SpringBootApplication
 @MapperScan(basePackages = {"cn.tjsanshao.blogserver.repository"})
+@EnableTransactionManagement
 public class BlogServerApplication {
 
     public static void main(String[] args) {
